49c2e68b8473ac82a61aad4d8d797fb0fffe5920
SkyWorld117 <alanzhu02@gmail.com>
2025-12-27 10:37:42 +0100

    Include HiGHS as dependency

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export HIGHS_DIR="/scratch/zyi/MyPackages/HiGHS"
-
 cmake -B build \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_BUILD_RPATH="$FROMAGER_ENV/system/lib;$FROMAGER_ENV/system/lib64" \
