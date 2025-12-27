540473c1825e19c6f2debaf1008a85f4c6d08f60
SkyWorld117 <alanzhu02@gmail.com>
2025-12-27 10:03:41 +0100

    Add compile commands

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ export HIGHS_DIR="/scratch/zyi/MyPackages/HiGHS"
 cmake -B build \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_BUILD_RPATH="$FROMAGER_ENV/system/lib;$FROMAGER_ENV/system/lib64" \
-    -DCMAKE_INSTALL_RPATH="$FROMAGER_ENV/system/lib;$FROMAGER_ENV/system/lib64"
+    -DCMAKE_INSTALL_RPATH="$FROMAGER_ENV/system/lib;$FROMAGER_ENV/system/lib64" \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build --parallel
