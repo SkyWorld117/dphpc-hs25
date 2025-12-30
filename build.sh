cf5e44ace703e85eb07beca7621b98575a36bceb
SkyWorld117 <alanzhu02@gmail.com>
2025-12-30 23:01:44 +0100

    More generic build system

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -Eeuo pipefail
+
 export CUDSS_DIR="/scratch/zyi/MyPackages/libcudss-0.7.1.4"
 
 cmake -B build \
@@ -8,6 +10,18 @@ cmake -B build \
     -DCMAKE_INSTALL_RPATH="$FROMAGER_ENV/system/lib;$FROMAGER_ENV/system/lib64" \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build --parallel
+
+# Generate .clangd file for clangd
+echo "CompileFlags:
+  Add:
+    -  --cuda-path=$FROMAGER_ENV/system/
+    -  --cuda-gpu-arch=sm_90
+    -  -L$FROMAGER_ENV/system/lib64
+    -  -I$FROMAGER_ENV/system/include
+    -  -I/scratch/zyi/MyPackages/libcudss-0.7.1.4/include
+    -  -I$PWD/src
+    -  -I$PWD/include
+" > .clangd
 
 python3 -c "
 import json
@@ -18,8 +32,8 @@ if os.path.exists(db_path):
     with open(db_path, 'r') as f:
         data = json.load(f)
     
-    target_file = '/scratch/sherrman/dphpc/dphpc-hs25/src/dual_simplex/phase2_dual.cu'
-    new_cmd = '/scratch/sherrman/.fromager/cellars/system/bin/nvcc -O3 -DNDEBUG -lineinfo -std=c++17 -Werror=cross-execution-space-call -Wno-deprecated-declarations -Wno-error=non-template-friend -fopenmp -lineinfo -x cu -c /scratch/sherrman/dphpc/dphpc-hs25/src/dual_simplex/phase2_dual.cu -o CMakeFiles/cuopt.dir/src/dual_simplex/phase2_dual.cu.o'
+    target_file = '$PWD/src/dual_simplex/phase2_dual.cu'
+    new_cmd = '/scratch/sherrman/.fromager/cellars/system/bin/nvcc -O3 -DNDEBUG -lineinfo -std=c++17 -Werror=cross-execution-space-call -Wno-deprecated-declarations -Wno-error=non-template-friend -fopenmp -lineinfo -x cu -c $PWD/src/dual_simplex/phase2_dual.cu -o CMakeFiles/cuopt.dir/src/dual_simplex/phase2_dual.cu.o'
     
     for entry in data:
         if entry['file'] == target_file:
@@ -28,3 +42,5 @@ if os.path.exists(db_path):
     with open(db_path, 'w') as f:
         json.dump(data, f, indent=2)
 "
+
+set +Eeuo pipefail
