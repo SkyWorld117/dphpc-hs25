b4be3ec2c05f713c43b03490cb63d697530c03ab
Sophia Herrmann <sophia.herrmann@inf.ethz.ch>
2025-12-30 14:19:57 +0100

    commit nvim stuff

--- build.sh
+++ build.sh
@@ -8,3 +8,23 @@ cmake -B build \
     -DCMAKE_INSTALL_RPATH="$FROMAGER_ENV/system/lib;$FROMAGER_ENV/system/lib64" \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build --parallel
+
+python3 -c "
+import json
+import os
+
+db_path = 'build/compile_commands.json'
+if os.path.exists(db_path):
+    with open(db_path, 'r') as f:
+        data = json.load(f)
+    
+    target_file = '/scratch/sherrman/dphpc/dphpc-hs25/src/dual_simplex/phase2_dual.cu'
+    new_cmd = '/scratch/sherrman/.fromager/cellars/system/bin/nvcc -O3 -DNDEBUG -lineinfo -std=c++17 -Werror=cross-execution-space-call -Wno-deprecated-declarations -Wno-error=non-template-friend -fopenmp -lineinfo -x cu -c /scratch/sherrman/dphpc/dphpc-hs25/src/dual_simplex/phase2_dual.cu -o CMakeFiles/cuopt.dir/src/dual_simplex/phase2_dual.cu.o'
+    
+    for entry in data:
+        if entry['file'] == target_file:
+            entry['command'] = new_cmd
+            
+    with open(db_path, 'w') as f:
+        json.dump(data, f, indent=2)
+"
