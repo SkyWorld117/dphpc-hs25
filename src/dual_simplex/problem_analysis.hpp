6d2f736c87c4bf25da947a9073df0121db517124
SkyWorld117 <alanzhu02@gmail.com>
2026-01-02 16:13:45 +0100

    Add more analysis

--- src/dual_simplex/problem_analysis.hpp
+++ src/dual_simplex/problem_analysis.hpp
@@ -9,31 +9,27 @@ template <typename i_t, typename f_t> class problem_analyzer_t {
   public:
     problem_analyzer_t(const lp_problem_t<i_t, f_t>& problem,
                        const simplex_solver_settings_t<i_t, f_t>& settings)
-        : lp_problem(problem), solver_settings(settings), preprocessed_A(0, 0, 0),
-          preprocessed_AT(0, 0, 0) {}
+        : lp_problem(problem), solver_settings(settings), preprocessed_A(0, 0, 0) {}
 
     void analyze();
+    void construct_preprocessed_A();
     void display_analysis() const;
 
     csc_matrix_t<i_t, f_t> preprocessed_A;
-    csc_matrix_t<i_t, f_t> preprocessed_AT;
+
+    i_t max_nnz_per_col = 0;
 
   private:
     const lp_problem_t<i_t, f_t>& lp_problem;
     const simplex_solver_settings_t<i_t, f_t>& solver_settings;
 
-    i_t max_nnz_per_row = 0;
-    i_t min_nnz_per_row = 0;
-    i_t max_nnz_per_col = 0;
     i_t min_nnz_per_col = 0;
-    f_t avg_nnz_per_row = 0.0;
     f_t avg_nnz_per_col = 0.0;
+    i_t median_nnz_per_col = 0;
 
     // Block size statistics:
     // smallest block size that is greater than 95%/99% of the rows/columns
-    i_t block_size_per_row_95 = 0;
     i_t block_size_per_col_95 = 0;
-    i_t block_size_per_row_99 = 0;
     i_t block_size_per_col_99 = 0;
 };
 
