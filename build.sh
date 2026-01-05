a7eb6b6ca5490e39ada7e4cc70a8ffdeace2d942
jupieper <4julianp@gmail.com>
2026-01-05 22:04:33 +0100

    For quick test run

--- build.sh
+++ build.sh
@@ -2,45 +2,51 @@
 
 set -Eeuo pipefail
 
-export CUDSS_DIR="/scratch/zyi/MyPackages/libcudss-0.7.1.4"
+export CUDSS_DIR="/home/jupieper/tools/libcudss-linux-x86_64-0.7.1.4_cuda12-archive"
 
+export CC="/cluster/data/gcc/11/bin/gcc"
+export CXX="/cluster/data/gcc/11/bin/g++"
+export CMAKE_CUDA_COMPILER="/cluster/data/cuda/12.8.1/bin/nvcc"
+
+# NOTE: Added CUDA Lineinfo here just in case
 cmake -B build \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_BUILD_RPATH="$FROMAGER_ENV/system/lib;$FROMAGER_ENV/system/lib64" \
-    -DCMAKE_INSTALL_RPATH="$FROMAGER_ENV/system/lib;$FROMAGER_ENV/system/lib64" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CUDA_LINEINFO=ON \
+    -DCMAKE_BUILD_RPATH="/usr/lib;/usr/lib64" \
+    -DCMAKE_INSTALL_RPATH="/usr/lib;/usr/lib64" \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build --parallel
 
 # Generate .clangd file for clangd
-echo "CompileFlags:
-  Add:
-    -  --cuda-path=$FROMAGER_ENV/system/
-    -  --cuda-gpu-arch=sm_90
-    -  -L$FROMAGER_ENV/system/lib64
-    -  -I$FROMAGER_ENV/system/include
-    -  -I$CUDSS_DIR/include
-    -  -I$PWD/src
-    -  -I$PWD/include
-" > .clangd
+# echo "CompileFlags:
+#   Add:
+#     -  --cuda-path=$FROMAGER_ENV/system/
+#     -  --cuda-gpu-arch=sm_90
+#     -  -L$FROMAGER_ENV/system/lib64
+#     -  -I$FROMAGER_ENV/system/include
+#     -  -I$CUDSS_DIR/include
+#     -  -I$PWD/src
+#     -  -I$PWD/include
+# " > .clangd
 
-python3 -c "
-import json
-import os
+# python3 -c "
+# import json
+# import os
 
-db_path = 'build/compile_commands.json'
-if os.path.exists(db_path):
-    with open(db_path, 'r') as f:
-        data = json.load(f)
+# db_path = 'build/compile_commands.json'
+# if os.path.exists(db_path):
+#     with open(db_path, 'r') as f:
+#         data = json.load(f)
     
-    target_file = '$PWD/src/dual_simplex/phase2_dual.cu'
-    new_cmd = '$FROMAGER_ENV/system/bin/nvcc -O3 -DNDEBUG -lineinfo -std=c++17 -Werror=cross-execution-space-call -Wno-deprecated-declarations -Wno-error=non-template-friend -fopenmp -lineinfo -x cu -c $PWD/src/dual_simplex/phase2_dual.cu -o CMakeFiles/cuopt.dir/src/dual_simplex/phase2_dual.cu.o'
+#     target_file = '$PWD/src/dual_simplex/phase2_dual.cu'
+#     new_cmd = '$FROMAGER_ENV/system/bin/nvcc -O3 -DNDEBUG -lineinfo -std=c++17 -Werror=cross-execution-space-call -Wno-deprecated-declarations -Wno-error=non-template-friend -fopenmp -lineinfo -x cu -c $PWD/src/dual_simplex/phase2_dual.cu -o CMakeFiles/cuopt.dir/src/dual_simplex/phase2_dual.cu.o'
     
-    for entry in data:
-        if entry['file'] == target_file:
-            entry['command'] = new_cmd
+#     for entry in data:
+#         if entry['file'] == target_file:
+#             entry['command'] = new_cmd
             
-    with open(db_path, 'w') as f:
-        json.dump(data, f, indent=2)
-"
+#     with open(db_path, 'w') as f:
+#         json.dump(data, f, indent=2)
+# "
 
 set +Eeuo pipefail
