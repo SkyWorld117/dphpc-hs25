b5c2fe3aacb9a6a4b6a376f89b02fed1512919d1
SkyWorld117 <alanzhu02@gmail.com>
2026-01-07 23:45:28 +0100

    Improve get at index (not significant)

--- src/dual_simplex/sparse_vector.cu
+++ src/dual_simplex/sparse_vector.cu
@@ -1,4 +1,5 @@
 #include <dual_simplex/sparse_vector.cuh>
+#include <thrust/binary_search.h>
 #include <thrust/device_ptr.h>
 #include <thrust/execution_policy.h>
 #include <thrust/functional.h>
@@ -38,19 +39,6 @@ __global__ void sparse_vector_squared_norm_kernel(i_t nz, const i_t *d_indices, 
     // Write result for this block to global memory
     if (tid == 0) {
         d_partial_sums[blockIdx.x] = sdata[0];
-    }
-}
-
-template <typename i_t, typename f_t>
-__global__ void sparse_vector_get_kernel(i_t nz, const i_t *d_indices, const f_t *d_values,
-                                         i_t query_index, f_t *d_result) {
-    i_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (idx < nz) {
-        if (d_indices[idx] == query_index) {
-            // Found the index, write the value to result
-            *d_result = d_values[idx];
-        }
     }
 }
 
@@ -161,25 +149,34 @@ template <typename i_t, typename f_t> void cu_vector<i_t, f_t>::squared_norm(f_t
 }
 
 template <typename i_t, typename f_t> f_t cu_vector<i_t, f_t>::get(i_t index) {
-    // If vector is empty or index out of bounds, return 0
+    // Early exit when empty or out-of-bounds
     if (nnz == 0 || index < 0 || index >= m) {
         return 0.0;
     }
-    
-    f_t *result;
-    CUDA_CALL_AND_CHECK(cudaMalloc(&result, sizeof(f_t)), "cudaMalloc result in cu_vector::get");
-    CUDA_CALL_AND_CHECK(cudaMemset(result, 0, sizeof(f_t)), "cudaMemset result in cu_vector::get");
 
-    const i_t block_size = 256;
-    i_t grid_size = (nnz + block_size - 1) / block_size;
-    sparse_vector_get_kernel<<<grid_size, block_size>>>(nnz, d_indices, d_values, index, result);
-    CUDA_CALL_AND_CHECK(cudaGetLastError(), "sparse_vector_get_kernel launch");
+    auto policy = thrust::cuda::par.on(0);
+    thrust::device_ptr<const i_t> idx_begin(d_indices);
+    thrust::device_ptr<const i_t> idx_end = idx_begin + nnz;
 
-    f_t h_result;
-    CUDA_CALL_AND_CHECK(cudaMemcpy(&h_result, result, sizeof(f_t), cudaMemcpyDeviceToHost),
-                        "cudaMemcpy h_result in cu_vector::get");
-    CUDA_CALL_AND_CHECK(cudaFree(result), "cudaFree result in cu_vector::get");
-    return h_result;
+    // Binary search on device to locate index (indices assumed sorted)
+    auto it = thrust::lower_bound(policy, idx_begin, idx_end, index);
+    const i_t pos = static_cast<i_t>(it - idx_begin);
+    if (pos == nnz) {
+        return 0.0;
+    }
+
+    i_t found_index = -1;
+    CUDA_CALL_AND_CHECK(cudaMemcpy(&found_index, d_indices + pos, sizeof(i_t),
+                                   cudaMemcpyDeviceToHost),
+                        "cudaMemcpy found_index in cu_vector::get");
+    if (found_index != index) {
+        return 0.0;
+    }
+
+    f_t value = 0.0;
+    CUDA_CALL_AND_CHECK(cudaMemcpy(&value, d_values + pos, sizeof(f_t), cudaMemcpyDeviceToHost),
+                        "cudaMemcpy value in cu_vector::get");
+    return value;
 }
 
 template class cu_vector<int, double>;
